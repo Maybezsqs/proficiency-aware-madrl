38a470d4220c27ace57026b7ac19dccfa005ec15
Flora <yuyim@126.com>
2019-09-02 18:07:54 -0400

    points

--- multiagent/environment.py
+++ multiagent/environment.py
@@ -269,19 +269,19 @@ class MultiAgentEnv(gym.Env):
             # and landmarks is different from agents in real robotic scenario
             
             ########
-            geom = rendering.make_polygon([(-0.01,0.01), (0.03,0.05), (0.06,0.08), (0.10,0.18), (-0.01,0.01)])
-            xform = rendering.Transform()
-            geom.set_color(0.09,0.3,1.0)
-            geom.add_attr(xform)
-            self.render_geoms.append(geom)
-            self.render_geoms_xform.append(xform)
+            #geom = rendering.make_polygon([(-0.01,0.01), (0.03,0.05), (0.06,0.08), (0.10,0.18), (-0.01,0.01)])
+            #xform = rendering.Transform()
+            #geom.set_color(0.09,0.3,1.0)
+            #geom.add_attr(xform)
+            #self.render_geoms.append(geom)
+            #self.render_geoms_xform.append(xform)
             ########
 
             for entity in self.world.entities:
                 #geom = rendering.make_circle(entity.size)
                 #xform = rendering.Transform()
                 if 'landmark' in entity.name:
-                    geom = rendering.make_polygon(building_coordinations[i])
+                    geom = rendering.make_polygon(building_coordinations[i] / 100)
                     #plist = [(0.045, 0.0), (-0.03011087728614863, -0.03344151714648273), (0.004703780847044385, -0.044753485291572304), (0.044016642033021254, -0.009356026086799155)]
                     #geom = rendering.make_polygon(plist)
                     xform = rendering.Transform()
