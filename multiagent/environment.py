20e94f7aef6dabc4dd62bccea65d89997cc62b9a
Flora <yuyim@126.com>
2019-09-02 15:10:32 -0400

    points

--- multiagent/environment.py
+++ multiagent/environment.py
@@ -274,8 +274,8 @@ class MultiAgentEnv(gym.Env):
                     geom.set_color(*entity.color, alpha=0.5)
                 elif 'landmark' in entity.name:
                     #geom = rendering.make_polygon(building_coordinations[i])
-                    list = [(0.045, 0.0), (-0.03011087728614863, -0.03344151714648273), (0.004703780847044385, -0.044753485291572304), (0.044016642033021254, -0.009356026086799155)]
-                    geom = rendering.make_polygon(list)
+                    plist = [(0.045, 0.0), (-0.03011087728614863, -0.03344151714648273), (0.004703780847044385, -0.044753485291572304), (0.044016642033021254, -0.009356026086799155)]
+                    geom = rendering.make_polygon(plist)
                     xform = rendering.Transform()
                     geom.set_color(*entity.color)
                 geom.add_attr(xform)
