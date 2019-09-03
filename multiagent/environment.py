9634afb8535432e6db565a39773f2d71977a2874
Flora <yuyim@126.com>
2019-09-04 03:13:14 +0800

    Some errors

--- multiagent/environment.py
+++ multiagent/environment.py
@@ -232,6 +232,7 @@ class MultiAgentEnv(gym.Env):
             # entities: agents + landmarks
             # and landmarks is different from agents in real robotic scenario
             i, j = 0, 0
+            print(i,j)
             for entity in self.world.entities:
                 #geom = rendering.make_circle(entity.size)
                 #xform = rendering.Transform()
