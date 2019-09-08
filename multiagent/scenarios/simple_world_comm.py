fc21dfd0e8d52004323d25bf8e04ec4a25d9174a
Flora <yuyim@126.com>
2019-09-08 16:52:46 -0400

    step length

--- multiagent/scenarios/simple_world_comm.py
+++ multiagent/scenarios/simple_world_comm.py
@@ -131,8 +131,8 @@ class Scenario(BaseScenario):
             landmark.color = np.array([0.6, 0.9, 0.6])
         # set random initial states
         # For experiment setting, I give three fixed positions for agents to start
-        red_chaser_init_pos = np.array([[-25.0,-41.25],[25.0,-41.25],[-25.0,41.25]])
-        green_escaper_init_pos = np.array([[3.0,-16.0],[0.0,0.0],[5.0,20.0],[-23.0,-13.0],[25.0,41.25]])
+        red_chaser_init_pos = np.array([[-22.0,-38.25],[222.0,-38.25],[-22.0,38.25]])
+        green_escaper_init_pos = np.array([[0.0,0.0],[3.0,-16.0],[5.0,20.0],[-23.0,-13.0],[25.0,41.25]])
         for i, agent in enumerate(world.agents):
             #agent.state.p_pos = np.random.uniform(-1, +1, world.dim_p)
             # TODO This 1 is expected to be changed as tests and experiments
@@ -204,11 +204,12 @@ class Scenario(BaseScenario):
         adversaries = self.adversaries(world)
         if shape:
             for adv in adversaries:
-                rew += 0.1 * np.sqrt(np.sum(np.square(agent.state.p_pos - adv.state.p_pos)))
+                rew += 0.1 * np.sqrt(np.sum(np.square(agent.state.p_pos - adv.state.p_pos))) # The further, the better
         if agent.collide:
             for a in adversaries:
                 # Penalty for being caught
                 if self.is_collision(a, agent):
+                    print("Caught :-(")
                     rew -= 5
         def bound(x):
             if x < 0.9:
@@ -220,10 +221,13 @@ class Scenario(BaseScenario):
         for p in range(world.dim_p):
             x = abs(agent.state.p_pos[p])
             rew -= 2 * bound(x)
+            if not bound(x) == 0:
+                print("bound")
 
         for food in world.food:
             if self.is_collision(agent, food):
                 rew += 2
+                print("food")
         rew += 0.05 * min([np.sqrt(np.sum(np.square(food.state.p_pos - agent.state.p_pos))) for food in world.food])
 
         return rew
@@ -235,12 +239,13 @@ class Scenario(BaseScenario):
         agents = self.good_agents(world)
         adversaries = self.adversaries(world)
         if shape:
-            rew -= 0.1 * min([np.sqrt(np.sum(np.square(a.state.p_pos - agent.state.p_pos))) for a in agents])
+            rew -= 0.1 * min([np.sqrt(np.sum(np.square(a.state.p_pos - agent.state.p_pos))) for a in agents]) # The further the worse
         if agent.collide:
             for ag in agents:
                 for adv in adversaries:
                     if self.is_collision(ag, adv):
                         rew += 5
+                        print("Caught :-)")
         return rew
 
     # Observation settings below is very important!! Keep in mind!! Need more modifications!!
